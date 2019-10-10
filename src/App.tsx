f20cfa69972f61f4e82b640b59682ec958fa3c26
Jack Nugent <jnugent@captechventures.com>
2019-10-09 21:59:40 -0400

    simplify front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
-import { subscribeToRoute } from "./api";
+import openSocket from "socket.io-client";
+const socket = openSocket("localhost:8000");
+
+const subscribeToRoute = (cb: any, route: number) => {
+  socket.on("estimates", (estimate: any) => cb(null, estimate));
+  socket.emit("subscribeToRoute", route, 100);
+};
 
 const App = () => {
   const [estimates, setEstimates] = useState("loading...");
   const [route, setRoute] = useState(3503);
 
-  subscribeToRoute((err: any, estimate: any) => setEstimates(estimate), 3503);
+  const changeSocket = (route: any) => {
+    setRoute(route);
+    socket.emit("subscribeToRoute", route, 100);
+  };
+
+  subscribeToRoute((err: any, estimate: any) => setEstimates(estimate), route);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>{estimates}</p>
-        <select value={route} onChange={(e: any) => setRoute(e.target.value)}>
-          <option value={3503}>east</option>
-          <option value={3504}>west</option>
+        <select
+          value={route}
+          onChange={(e: any) => changeSocket(e.target.value)}
+        >
+          <option value={3504}>east</option>
+          <option value={3503}>west</option>
         </select>
       </header>
     </div>
--- src/api.tsx
+++
@@ -1,8 +0,0 @@
-import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:8000");
-
-const subscribeToRoute = (cb: any, route: number) => {
-  socket.on("estimates", (estimate: any) => cb(null, estimate));
-  socket.emit("subscribeToRoute", route, 100);
-};
-export { subscribeToRoute };
