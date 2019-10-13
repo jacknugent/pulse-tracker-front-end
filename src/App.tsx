c549fc003788755bd0cc048abd90a8a575e5a7ad
Jack Nugent <jnugent@captechventures.com>
2019-10-13 10:42:10 -0400

    console.log bug still happening

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
 import React, { useState, useEffect } from "react";
 import openSocket from "socket.io-client";
-const socket = openSocket("https://9195fabe.ngrok.io");
+import styled from "@emotion/styled";
+import { Global, css } from "@emotion/core";
+
+const socket = openSocket("http://9195fabe.ngrok.io");
+
+const globalStyles = css`
+  html,
+  body {
+    height: 100%;
+  }
+  #root {
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  height: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+const Title = styled.h1`
+  text-align: center;
+  font-size: 8vw;
+  margin: 0;
+`;
+
+const Estimates = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-size: 7vw;
+`;
+
+const Select = styled.select`
+  margin: 0 auto;
+  display: flex;
+  height: 50px;
+  font-size: 20px;
+`;
 
 const App = () => {
-  const [estimates, setEstimates] = useState("loading...");
+  const [stopName, setStopName] = useState("Loading...");
+  const [estimates, setEstimates] = useState([]);
   const [route, setRoute] = useState(3504);
 
   const changeSocket = (newRoute: any) => {
-    console.log("old route", route);
     setRoute(previousState => {
       socket.emit("leaveRoom", previousState);
       socket.emit("room", newRoute);
@@ -16,94 +55,35 @@ const App = () => {
   };
 
   useEffect(() => {
-    // waiting for an event
-    socket.on("connect", function() {
-      socket.emit("room", route);
-    });
-  }, []);
+    socket.emit("room", route);
+  }, [route]);
 
   socket.on("estimate", function(data: any) {
-    setEstimates(data);
+    console.log(data);
+    if (data) {
+      const stopInfo = JSON.parse(data);
+      setStopName(stopInfo.stpnm);
+      setEstimates(stopInfo.estimates);
+    } else {
+      setEstimates([]);
+    }
   });
 
   return (
-    <div>
-      <header>
-        <p>{estimates}</p>
-        <select
-          value={route}
-          onChange={(e: any) => changeSocket(e.target.value)}
-        >
-          <option value={3504}>east</option>
-          <option value={3503}>west</option>
-        </select>
-      </header>
-    </div>
+    <Container>
+      <Global styles={globalStyles} />
+      <Title>{stopName}</Title>
+      <Estimates>
+        {estimates.map((estimate: any, i: number) => (
+          <div key={i}>{estimate}</div>
+        ))}
+      </Estimates>
+      <Select value={route} onChange={(e: any) => changeSocket(e.target.value)}>
+        <option value={3504}>Scott's Addition East</option>
+        <option value={3503}>Scott's Addition West</option>
+      </Select>
+    </Container>
   );
 };
 
 export default App;
-
-// import React, { useState, useEffect } from "react"
-// import Layout from "../components/layout"
-// import openSocket from "socket.io-client"
-// import styled from "@emotion/styled"
-// const socket = openSocket(process.env.NGROK_ID)
-
-// const Container = styled.div``
-// const Title = styled.h1`
-//   text-align: center;
-//   font-size: 8vw;
-// `
-
-// const Estimates = styled.div`
-//   display: flex;
-//   justify-content: space-around;
-//   font-size: 7vw;
-// `
-
-// const PulseDemo = (props: any) => {
-//   const [stopName, setStopName] = useState("Loading...")
-//   const [estimates, setEstimates] = useState([])
-//   const [route, setRoute] = useState(3504)
-
-//   const changeSocket = (newRoute: any) => {
-//     setRoute(previousState => {
-//       socket.emit("leaveRoom", previousState)
-//       socket.emit("room", newRoute)
-//       return newRoute
-//     })
-//   }
-
-//   useEffect(() => {
-//     socket.emit("room", route)
-//   }, [])
-
-//   socket.on("estimate", function(data: any) {
-//     console.log(data)
-//     if (data) {
-//       const stopInfo = JSON.parse(data)
-//       setStopName(stopInfo.stpnm)
-//       setEstimates(stopInfo.estimates)
-//     } else {
-//       setEstimates([])
-//     }
-//   })
-
-//   return (
-//     <Container>
-//       <Title>{stopName}</Title>
-//       <Estimates>
-//         {estimates.map(estimate => (
-//           <div>{estimate}</div>
-//         ))}
-//       </Estimates>
-//       <select value={route} onChange={(e: any) => changeSocket(e.target.value)}>
-//         <option value={3504}>Scott's Addition East</option>
-//         <option value={3503}>Scott's Addition West</option>
-//       </select>
-//     </Container>
-//   )
-// }
-
-// export default PulseDemo
